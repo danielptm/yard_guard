a6df16a93ed92dfec652ddfbfd4a9d1ae3000ff7
Daniel <danielptm@me.com>
2025-12-27 10:31:49 -0800

    added more xacro files

--- build_xacro.sh
+++ build_xacro.sh
@@ -1,1 +1,1 @@
-ros2 run xacro xacro -o /home/daniel/projects/yard_guard/src/yardguard_description/urdf/xacro/yardguard.xacro /home/daniel/projects/yard_guard/src/yardguard_description/urdf/yardguard.urdf;
+ros2 run xacro xacro -o /home/daniel/projects/yard_guard/src/yardguard_description/urdf/output/yardguard.urdf /home/daniel/projects/yard_guard/src/yardguard_description/urdf/yardguard.xacro;
